cbea09e4790c502dc13ed4dbcc550bb3fd7a41df
HarisHidayatul <Hidayatulloh.haris@gmail.com>
2023-04-13 09:22:13 +0700

    Memperbaiki patty

--- app/Http/Controllers/reimburseController.php
+++ app/Http/Controllers/reimburseController.php
@@ -467,14 +467,12 @@ class reimburseController extends Controller
     public function updateAllHistoryOutlet()
     {
         $outletAll = doutlet::all();
-        for($i =0;$i<$outletAll->count();$i++){
-            for ($j = 0; $j < $outletAll->count(); $j++) {
-                echo $outletAll[$j]['Nama Store'];
-                echo '<br>';
-                $this->updateAllHistory($outletAll[$j]->id);
-                echo '<br>';
-                echo '<br>';
-            }
+        for ($j = 0; $j < $outletAll->count(); $j++) {
+            echo $outletAll[$j]['Nama Store'];
+            echo '<br>';
+            $this->updateAllHistory($outletAll[$j]->id);
+            echo '<br>';
+            echo '<br>';
         }
     }
     public function updateAllHistory($idOutlet)
